081d0a1400aa8d696c06f63eacf911a4aa987048
Abdullah Al Mubin <amttawsik99@gmail.com>
2022-10-11 20:31:16 +0600

    edit some responsivnes

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -6,7 +6,7 @@ const Statistics = () => {
     return (
         <div>
             <h1 className='text-center font-extrabold text-3xl text-amber-400 mb-40'>The Chart is Here!!!</h1>
-            <LineChart  className='container mx-auto mt-10 pb-5' width={500} height={400} data={data}>
+            <LineChart  className='container md:mx-auto mt-10 pb-5 ml-0' width={300} height={300} data={data}>
                 <Line type="monotone" dataKey="total" stroke="#FF0000"></Line>
                 <XAxis dataKey="name"></XAxis>
                 <YAxis dataKey="total"></YAxis>
