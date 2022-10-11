358d3c7d01f5a112323a61316712851cb7c8bf59
Abdullah Al Mubin <amttawsik99@gmail.com>
2022-10-11 14:10:33 +0600

    Chart done

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -6,8 +6,8 @@ const Statistics = () => {
     console.log(data)
     return (
         <div>
-            <h1>The Chart is Here!!!</h1>
-            <LineChart  className='container mx-auto mt-10' width={500} height={400} data={data}>
+            <h1 className='text-center font-extrabold text-3xl text-amber-400 mb-40'>The Chart is Here!!!</h1>
+            <LineChart  className='container mx-auto mt-10 pb-5' width={500} height={400} data={data}>
                 <Line type="monotone" dataKey="total" stroke="#FF0000"></Line>
                 <XAxis dataKey="name"></XAxis>
                 <YAxis dataKey="total"></YAxis>
